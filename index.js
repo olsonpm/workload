82e7cdade7f370b247bf0ebbf85ccbe5e92374ae
Philip Olson <phil.olson@swarmsolutions.com>
2019-09-09 00:54:05 -0500

    add average repsonse time

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var weighted = require('weighted')
 var maybe = require('mostly-working-hours')
 var expand = require('brace-expansion')
 var weekend = require('is-it-weekend')
+var _mean = require('lodash/mean')
 var pkg = require('./package')
 
 var USER_AGENT = pkg.name + '/' + pkg.version
@@ -23,13 +24,14 @@ function Workload (opts) {
   var interval = Math.round(1000 / ((opts.max || 12) / 60)) // default to max 12 requests per minute
   var filters = opts.filters || [opts.filter || function (_, cb) { cb() }]
   this._defaultHeaders = opts.headers
+  this._responseTimes = []
 
   var weights = opts.requests.map(function (req) {
     return req.weight || 1
   })
 
   this._timer = setInterval(function () {
-    var req = xtend({}, weighted.select(opts.requests, weights))
+    var req = xtend({ _start: Date.now() }, weighted.select(opts.requests, weights))
     iterator(req)
   }, interval)
 
@@ -62,6 +64,8 @@ Workload.stdFilters = {
 
 Workload.prototype.stop = function stop () {
   clearInterval(this._timer)
+
+  return { avgResponseTime: _mean(this._responseTimes) };
 }
 
 Workload.prototype._visit = function _visit (req) {
@@ -70,6 +74,7 @@ Workload.prototype._visit = function _visit (req) {
   req.headers = xtend({'user-agent': USER_AGENT}, this._defaultHeaders, req.headers)
 
   request(req, function (err, res, body) {
+    self._responseTimes.push(Date.now() - req._start);
     if (err) return self.emit('error', err)
     self.emit('visit', {
       request: req,
